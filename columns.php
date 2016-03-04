4da68f194b2ef033294332f2bc025f9125190df4
Stefan Käsche <stefan.kaesche@artcom-venture.de>
2016-03-04 09:57:38 +0100

    gulp

--- columns.php
+++ columns.php
@@ -4,48 +4,51 @@
  * Plugin Name: Columns
  * Plugin URI: https://github.com/artcomventure/wordpress-plugin-columns
  * Description: Extends WP Editor with columns.
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: artcom venture GmbH
  * Author URI: http://www.artcom-venture.de/
  */
 
-// ...
-add_action( 'admin_head', 'columns_wpe_button' );
-function columns_wpe_button() {
-	// check if user has edit permission
+/**
+ * Admin.
+ */
+add_action( 'admin_head', 'columns__admin_head' );
+function columns__admin_head() {
+	// check if user has permission to edit posts
 	if ( ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) )
-	     // allow to use wysiwyg
+	     // ... and is allowed to use wysiwyg
 	     || ! get_user_option( 'rich_editing' ) == 'true'
 	) {
 		return;
 	}
 
-	add_editor_style( plugins_url( '/css/columns.css?' . time(), __FILE__ ) );
-	add_editor_style( plugins_url( '/css/columns.admin.css?' . time(), __FILE__ ) );
-	wp_enqueue_style( 'columns-admin', plugins_url( '/css/editor.css', __FILE__ ) );
+	// add styles to editor
+	add_editor_style( plugins_url( '/css/columns.min.css?' . time(), __FILE__ ) );
+	add_editor_style( plugins_url( '/css/columns.admin.min.css?' . time(), __FILE__ ) );
 
+	// enqueue button style
+	wp_enqueue_style( 'columns-admin', plugins_url( '/css/editor.min.css', __FILE__ ) );
+
+	// add plugin js
 	add_filter( 'mce_external_plugins', 'columns__mce_external_plugins' );
+	// add columns button to editor
 	add_filter( 'mce_buttons', 'columns__mce_buttons' );
 }
 
 /**
- * Enqueue scripts and styles.
+ * Enqueue default columns styles.
  */
 add_action( 'wp_enqueue_scripts', 'columns_enqueue_scripts' );
 function columns_enqueue_scripts() {
-	if ( is_admin() ) {
-		return;
-	}
-
-	wp_enqueue_style( 'columns', plugins_url( '/css/columns.css', __FILE__ ) );
+	wp_enqueue_style( 'columns', plugins_url( '/css/columns.min.css', __FILE__ ) );
 }
 
 /**
- * ...
+ * Add plugin js.
  *
- * @param $plugin_array
+ * @param array $plugin_array
  *
- * @return mixed
+ * @return array
  */
 function columns__mce_external_plugins( $plugin_array ) {
 	$plugin_array['columns'] = WP_PLUGIN_URL . '/columns/js/columns.admin.min.js';
@@ -54,23 +57,15 @@ function columns__mce_external_plugins( $plugin_array ) {
 }
 
 /**
- * Register column button.
+ * Add columns button to editor.
  *
  * @param array $buttons
  *
  * @return array
  */
 function columns__mce_buttons( $buttons ) {
-	// add columns button
+	// to front
 	array_unshift( $buttons, 'columns' );
-
-	// ...
-	foreach ( array( 'wp_more', 'wp_adv' ) as $button ) {
-		if ( $index = array_search( $button, $buttons ) ) {
-			unset( $buttons[ $index ] );
-			array_push( $buttons, $button );
-		}
-	}
 
 	return $buttons;
 }
